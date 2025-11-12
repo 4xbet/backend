8929825163c98c4e701df1f69117a64a115e4279
Van1s1mys <malkovsin@gmail.com>
2025-11-12 15:28:16 +0300

    fix

--- matches_service/app/crud.py
+++ matches_service/app/crud.py
@@ -16,7 +16,9 @@ async def get_match(db: AsyncSession, match_id: int):
 
 
 async def get_matches(db: AsyncSession, skip: int = 0, limit: int = 100):
-    result = await db.execute(select(models.Match).offset(skip).limit(limit))
+    result = await db.execute(
+        select(models.Match).options(selectinload(models.Match.odds)).offset(skip).limit(limit)
+    )
     return result.scalars().all()
 
 
